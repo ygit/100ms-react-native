c450834e75445623a62d03f99f4b11a1c1593707
Yogesh Singh <yogesh.singh.2810@gmail.com>
2021-07-15 23:34:30 +0530

    WIP: build fails

--- example/android/app/src/main/java/com/rnhms/MainActivity.java
+++ example/android/app/src/main/java/com/rnhms/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.reactnativehmssdk;
+package com.rnhms;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "HmssdkExample";
+    return "RNHMS";
   }
 }
